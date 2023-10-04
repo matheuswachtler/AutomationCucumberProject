3230b7ffab572c8a2b61e2fb874f771e4d685968
Matheus <matheus.wachtler@outlook.com>
2023-10-04 13:21:49 -0300

    Add new Class.

---
+++ src/test/java/pages/CartPage.java
@@ -0,0 +1,11 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+
+public class CartPage extends BasePage{
+    public CartPage(WebDriver webDriver) {
+        super(webDriver);
+        PageFactory.initElements(webDriver,this);
+    }
+}
